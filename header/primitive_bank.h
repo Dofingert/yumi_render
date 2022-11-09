2a59180ba4e9af38d98d46a2e78ea2a65ebf5e1a
Dofingert <dofingert@gmail.com>
2022-11-09 12:27:06 +0800

    完善：完善多线程primitive_fifo

--- header/primitive_bank.h
+++ header/primitive_bank.h
@@ -5,9 +5,37 @@
 #ifndef SOFTWARE_RASTERIZER_HEADER_PRIMITIVE_BANK_H_
 #define SOFTWARE_RASTERIZER_HEADER_PRIMITIVE_BANK_H_
 
-struct primitive_input_t {
+#define PRIMITIVE_FIFO_SIZE 64
+
+#include <mutex>
+#include <condition_variable>
+#include "render_type.h"
+struct PrimitiveInput {
   unsigned char index[3]; // Sub-index, just used to index in vertex_bank
   unsigned char flag;     // reserve for further usage.
 };
+
+struct Primitive {
+  vec3f pos[3];
+  vec3f normal[3];
+  vec2s uv[3];
+}; // 21 Word= 84 bytes
+
+class PrimitiveFifo {
+  unsigned int fifo_head;
+  bool is_killed = false;
+  std::mutex modify;
+  std::condition_variable cond_producer;
+  std::condition_variable cond_consumer;
+  Primitive slot[PRIMITIVE_FIFO_SIZE];
+  unsigned int fifo_end;
+
+  int push(const Primitive &input_ref);
+  void kill(); // Called by push side.
+  int pop(Primitive *target_buf);
+};
+// Warning, this module may be used in multiple-thread.
+// But fortunately, not need a mutex_lock.
+// More over, We put fifo_head and fifo_end on the two side of fifo
 
 #endif //SOFTWARE_RASTERIZER_HEADER_PRIMITIVE_BANK_H_
